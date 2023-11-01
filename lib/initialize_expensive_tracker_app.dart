0fc0f2c71ada6724c26633ca810bcdbbab9e2ab7
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-01 10:22:21 +0530

    create the app initialize screen.

---
+++ lib/initialize_expensive_tracker_app.dart
@@ -0,0 +1,10 @@
+import 'package:my_projects/utils/common_exports.dart';
+
+class InitializeExpensiveTrackerApp extends StatelessWidget {
+  const InitializeExpensiveTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
