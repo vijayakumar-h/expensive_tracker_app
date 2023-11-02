2e4d6cf917aedce6f34c25ac71607ad191f562fa
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-02 22:40:44 +0530

    added the light theme inside the application.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const ExpensiveTrackerApp());
 }
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 class ExpensiveTrackerApp extends StatelessWidget {
   const ExpensiveTrackerApp({super.key});
 
@@ -11,7 +15,29 @@ class ExpensiveTrackerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+              ),
+            ),
+      ),
       navigatorKey: NavigationServices.navigateKey,
       initialRoute: Routes.initializeExpensiveTracker,
       onGenerateRoute: NavigationServices().generateRoute,
--- lib/widgets/expensive_list/expense_list.dart
+++ lib/widgets/expensive_list/expense_list.dart
@@ -20,6 +20,12 @@ class ExpenseList extends StatelessWidget {
           onDismissed: (direction) {
             onRemoveExpense(expenses[index]);
           },
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+            margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+            ),
+          ),
           key: ValueKey(expenses[index]),
           child: ExpenseItem(expense: expenses[index])),
     );
